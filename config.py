650843bd4fcd6c8fcaaaaa03f7e1fa400a711aa8
igupta967937 <61583813+igupta967937@users.noreply.github.com>
2020-07-30 16:39:53 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '562c8734de61f7d1cf886cb5aa0a197b'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'ishaan4newacc@gmail.com'  # Dummy Value
+    MAIL_PASSWORD = 'ishaan883103665'  # Dummy Value
